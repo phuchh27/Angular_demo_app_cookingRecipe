e80311c028f16768a42d90700d9b993de48c78fa
phuchh27 <phuchh27@gmail.com>
2023-06-08 11:43:16 +0700

    Angular-basic-final

---
+++ cookingRecipe/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css'],
+})
+export class RecipesListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+  ];
+}
