a672944a41aeabba0441f0ae0f35e6222bcf95b0
phuchh27 <phuchh27@gmail.com>
2023-06-18 18:46:57 +0700

    inject service

--- cookingRecipe/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ cookingRecipe/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -4,31 +4,29 @@ import {
   ElementRef,
   ViewChild,
   EventEmitter,
-  Output
+  Output,
 } from '@angular/core';
 
-import { Ingredient } from '../../shared/ingredient.model';
+import { Ingredient } from '../../../models/ingredient.model';
+import { ShoppingListService } from 'src/services/shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
   @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onAddItem() {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.slService.addIngredient(newIngredient);
   }
-
 }
